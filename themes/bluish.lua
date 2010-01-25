a61284fa242ac83335da9ba44bfda6a30d7b36a4
Aktau <nicolashillegeer@gmail.com>
2010-01-25 16:41:07 +0100

    all current files in ~/.config/awesome added

---
+++ themes/bluish.lua
@@ -0,0 +1,38 @@
+theme = {}
+
+theme.font                    = 'ProggyTinyTT 8'
+
+theme.bg_normal               = '#000000'
+theme.fg_normal               = '#ECECEC'
+theme.border_normal           = '#333333'
+
+theme.bg_focus                = '#000000'
+theme.fg_focus                = '#0B8BFF'
+theme.border_focus            = '#0775A8'
+
+theme.bg_urgent               = '#000000'
+theme.fg_urgent               = '#FFFFAF'
+
+theme.bg_minimize             = '#222222'
+theme.fg_minimize             = '#666666'
+
+theme.border_width            = '1'
+theme.border_marked           = '#D6156C'
+
+theme.taglist_squares         = 'true'
+theme.taglist_squares_sel     = '/usr/share/awesome/themes/default/taglist/squarefw.png'
+theme.taglist_squares_unsel   = '/usr/share/awesome/themes/default/taglist/squarew.png'
+
+theme.titlebar_close_button   = 'false'
+
+theme.wallpaper_cmd           = { 'awsetbg /home/aktau/documents/wallpapers/bluebling.jpg' }
+
+theme.layout_floating         = '/home/aktau/.config/awesome/icons/layouts/floating.png'
+theme.layout_magnifier        = '/home/aktau/.config/awesome/icons/layouts/magnifier.png'
+theme.layout_max              = '/home/aktau/.config/awesome/icons/layouts/max.png'
+theme.layout_tilebottom       = '/home/aktau/.config/awesome/icons/layouts/tilebottom.png'
+theme.layout_tileleft         = '/home/aktau/.config/awesome/icons/layouts/tileleft.png'
+theme.layout_tile             = '/home/aktau/.config/awesome/icons/layouts/tile.png'
+theme.layout_tiletop          = '/home/aktau/.config/awesome/icons/layouts/tiletop.png'
+
+return theme
